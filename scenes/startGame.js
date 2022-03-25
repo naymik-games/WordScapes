6439d8b7b0314c139b3d72dcd71886d5a2272d93
naymik-games <naymik@gmail.com>
2022-03-25 15:57:22 -0400

    level start

--- scenes/startGame.js
+++ scenes/startGame.js
@@ -11,8 +11,10 @@ class home extends Phaser.Scene {
       gameData = defaultData;
     }
     onLevel = gameData.level;
+    var tempL = onLevel + 1;
+    ///this.levelText = this.add.bitmapText(200, 75, 'clarendon', 'Level ' + tempL, 120).setOrigin(0, .5).setTint(0xffffff).setMaxWidth(700);
 
-    this.start = this.add.text(game.config.width / 2, 660, 'play', { font: '88px Arial', fill: '#ffffff' }).setOrigin(.5);
+    this.start = this.add.text(game.config.width / 2, 660, 'Level ' + tempL, { font: '88px Arial', fill: '#ffffff' }).setOrigin(.5);
     this.start.setInteractive();
     this.start.on('pointerdown', function () {
       this.scene.start("PlayGame", { level: 0 });
